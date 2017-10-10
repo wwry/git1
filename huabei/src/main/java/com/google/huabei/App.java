361d3c98481ae7c29ffadec4d81103fd584dcd42
wwey <63837847@qq.com>
2017-10-10 16:26:49 +0800

    a

---
+++ huabei/src/main/java/com/google/huabei/App.java
@@ -0,0 +1,13 @@
+package com.google.huabei;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
